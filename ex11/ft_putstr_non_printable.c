914c524786703704018053b88db9b4d050a07bb6
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2023-03-07 18:39:29 +0000

    feat: Adds the folder 'ex11' and its file 'ft_putstr_non_printable.c'.

---
+++ ex11/ft_putstr_non_printable.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_non_printable.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arabelo- <arabelo-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/07 16:57:52 by arabelo-          #+#    #+#             */
+/*   Updated: 2023/03/07 18:38:48 by arabelo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr_non_printable(char *str)
+{
+	char const	*hex_table;
+	int			index;
+
+	hex_table = "0123456789abcdef";
+	index = 0;
+	while (str[index])
+	{
+		if (str[index] < 32 || str[index] > 126)
+		{
+			ft_putchar('\\');
+			ft_putchar(hex_table[str[index] / 16]);
+			ft_putchar(hex_table[str[index] % 16]);
+		}
+		else
+			ft_putchar(str[index]);
+		index++;
+	}
+}
