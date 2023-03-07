f8c97bedd48690a17458f012566f2b5eea4ff68f
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2023-03-07 09:34:38 +0000

    feat: Adds the folder 'ex01' with its file 'ft_strncpy.c'.

---
+++ ex01/ft_strncpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arabelo- <arabelo-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/07 08:48:27 by arabelo-          #+#    #+#             */
+/*   Updated: 2023/03/07 09:34:05 by arabelo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	int	index;
+
+	index = 0;
+	while (src[index] != '\0' && index < n)
+	{
+		dest[index] = src[index];
+		index++;
+	}
+	while (index < n)
+	{
+		dest[index] = '\0';
+		index++;
+		printf("%d\n", index);
+	}
+	return (dest);
+}
